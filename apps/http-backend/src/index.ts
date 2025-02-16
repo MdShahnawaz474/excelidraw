464c568cc08a6aa3e379a389fca9286d10e0feab
MD shahnawaz <mdshahnawazm474>
2025-02-16 19:14:16 +0530

    added ws logic and middlewares

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,37 @@
+
+import express from "express";
+import jwt from "jsonwebtoken"
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
+const app = express()
+
+
+app.post("/signup",(req,res)=>{
+
+})
+
+
+app.post("/signin", (req,res)=>{
+    const userId = 1;
+
+   const token = jwt.sign({
+        userId
+    }, JWT_SECRET)
+})
+
+
+app.post("/room", middleware, (req,res)=>{
+    // DB Call
+    res.json({
+        roomId:123
+    })
+})
+
+
+
+
+
+app.listen(3001, ()=>{
+    console.log("Backend is listening PORT 3001 ");
+    
+});
