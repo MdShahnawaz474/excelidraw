75057678d8ca61a3df17dd1d17f1f63af501a38a
MD shahnawaz <mdshahnawazm474>
2025-02-16 22:29:09 +0530

    added common package for zod validation

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,37 +1,61 @@
-
 import express from "express";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/common-backend/config";
 import { middleware } from "./middleware";
-const app = express()
+import { CreateRoomSchema, CreateUserSchema, SiginSchema } from "@repo/common/types";
+const app = express();
 
-
-app.post("/signup",(req,res)=>{
-
-})
-
-
-app.post("/signin", (req,res)=>{
-    const userId = 1;
-
-   const token = jwt.sign({
-        userId
-    }, JWT_SECRET)
-})
-
-
-app.post("/room", middleware, (req,res)=>{
-    // DB Call
-    res.json({
-        roomId:123
+app.post("/signup", (req, res) => {
+  const data = CreateUserSchema.safeParse(req.body);
+  if(!data.success){
+     res.json({
+        message:"Incorrect Inputs"
     })
-})
+    return;
+  }
 
+  res.json({
+    userId:"1234"
+  })
+});
 
+app.post("/signin", (req, res) => {
+  const userId = 1;
+  const data = SiginSchema.safeParse(req.body)
+  if(!data){
+    res.json({
+        message:"Incorrect inputs"
+    })
+    return;
 
+  }
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET
+  );
+  res.json({
+    token
+  })
+});
 
+app.post("/room", middleware, (req, res) => {
+  
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data){
+        res.json({
+            message:"Incorrect Inputs"
+        })
+        return;
+    }
+  
+    // DB Call
+  res.json({
+    roomId: 123,
+  });
+});
 
-app.listen(3001, ()=>{
-    console.log("Backend is listening PORT 3001 ");
-    
+app.listen(3001, () => {
+  console.log("Backend is listening PORT 3001 ");
 });
